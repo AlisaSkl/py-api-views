ee508037c05743ea02b4c586fa6516f42908bed7
Alisa <alisasklrv@gmail.com>
2024-06-01 17:33:01 +0300

    solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -29,11 +29,11 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema-halls/", cinema_halls_list, name="cinemahall-list"),
+    path("cinema_halls/", cinema_halls_list, name="cinema-hall-list"),
     path(
-        "cinema-halls/<int:pk>/",
+        "cinema_halls/<int:pk>/",
         cinema_halls_detail,
-        name="cinemahall-detail"
+        name="cinema-hall-detail"
     ),
     path("", include(router.urls))
 ]
